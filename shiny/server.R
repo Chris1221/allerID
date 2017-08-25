0f6adb3be3e8cda97968b4658fedda43f32767c4
Chris1221 <chris.c.1221@gmail.com>
2017-08-25 18:44:33 -0400

    actually working pretty well

--- shiny/server.R
+++ shiny/server.R
@@ -4,56 +4,76 @@ library(magrittr)
 library(tm)
 
 function(input, output, session) {
-	
-		nonveganTDM <- {
+
+		allergyTDM <- reactive({
 			
-		raw_go <- readLines("../data/not_vegan.txt")	
-		raw_go <- iconv(raw_go,"WINDOWS-1252","UTF-8") #this might not be a silver bullet, check the encoding
-		raw_go <- raw_go[which(raw_go!="")]
+			raw_go <- readLines("../data/init.txt")
 
-		raw_go <- unlist(strsplit(raw_go, ","))
-		doc.vec <- VectorSource(raw_go)
-		doc.corpus <- Corpus(doc.vec)
-		raw.corpus <- doc.corpus # for use later	
-		doc.corpus <- tm_map(doc.corpus, content_transformer(tolower))
-		doc.corpus <- tm_map(doc.corpus, content_transformer(replaceExpressions))
-		doc.corpus <- tm_map(doc.corpus, removePunctuation)
-		doc.corpus <- tm_map(doc.corpus, removeNumbers)
-		doc.corpus <- tm_map(doc.corpus, removeWords, stopwords("english"))
-		doc.corpus <- tm_map(doc.corpus, stemDocument)
-		doc.corpus <- tm_map(doc.corpus, stripWhitespace)
+			if( input$milk ) {
+				#raw_go <- c(raw_go, readLines("data/milk.txt"))
+			} else if( as.factor( input$vegan ) ) {
+				raw_go <- c(raw_go, readLines("../data/not_vegan.txt"))
+			}
+				
+			raw_go <- iconv(raw_go,"WINDOWS-1252","UTF-8") #this might not be a silver bullet, check the encoding
+			raw_go <- raw_go[which(raw_go!="")]
 
-		TermDocumentMatrix(doc.corpus) %>% as.matrix() %>%
-			as.data.frame ->
+			raw_go <- unlist(strsplit(raw_go, ","))
+			doc.vec <- VectorSource(raw_go)
+			doc.corpus <- Corpus(doc.vec)
+			raw.corpus <- doc.corpus # for use later	
+			doc.corpus <- tm_map(doc.corpus, content_transformer(tolower))
+			doc.corpus <- tm_map(doc.corpus, content_transformer(replaceExpressions))
+			doc.corpus <- tm_map(doc.corpus, removePunctuation)
+			doc.corpus <- tm_map(doc.corpus, removeNumbers)
+			doc.corpus <- tm_map(doc.corpus, removeWords, stopwords("english"))
+			doc.corpus <- tm_map(doc.corpus, stemDocument)
+			doc.corpus <- tm_map(doc.corpus, stripWhitespace)
+
+			TermDocumentMatrix(doc.corpus) %>% as.matrix() %>%
+				as.data.frame ->
+				TDM.go.df
+			
+			sub <- gsub(" ", "_", x = raw_go)
+			sub <- gsub("-", "_", x = sub)
+
+			colnames(TDM.go.df) <- sub
+
 			TDM.go.df
-		
-		sub <- gsub(" ", "_", x = raw_go)
-		sub <- gsub("-", "_", x = sub)
-
-		colnames(TDM.go.df) <- sub
-
-		TDM.go.df
-	}
-
+	})
+	
 	justString <- reactive(unlist(strsplit(input$ingredients, ", ")))
 
 	out <- reactive({
 		goldi::goldi(
 		doc = justString(),
 		#terms = c("peanut", "tallow"),
-		term_tdm = nonveganTDM,
+		term_tdm = allergyTDM(),
 		lims = c(0,1,2,3),
 		output = "../output.txt",
 		log = "../log.txt",
 		object = TRUE,
 		reader = "local") %>%
-	as.data.frame
+	as.data.frame -> goldi_output
 
+	# Process this to get the source
+	# 	This is pretty damn messy
+	
+	goldi_output$Source <- NA
+
+	for( i in 1:nrow(goldi_output) ){
+		if(goldi_output[i, 1] %in% readLines("../data/not_vegan.txt") ) goldi_output[i, "Source"] <- "https://www.peta.org/living/other/animal-ingredients-list/"
+
+		if(goldi_output[i, 1] %in% readLines("../data/init.txt") ) goldi_output[i, "Source"] <- "Jim Halpert"
+
+
+	}
+	goldi_output
 	})
 	
 	output$table <- DT::renderDataTable({
 		DT::datatable( 
 			out()
-		)
-	})
+			)
+	}, escape = F)
 }
--- shiny/ui.R
+++ shiny/ui.R
@@ -4,9 +4,24 @@ pageWithSidebar(
 
   headerPanel('Tool to identify non-vegan ingredients in cosmetics.'),
   sidebarPanel(
-	textInput("ingredients", "Paste ingredients here, seperated by commas.", value = "peanut")
+	h2("Which allergens?"),
+	p("Use the boxes below to select which allergens you're looking to avoid. You can select as many or as few as you want. Each comes from different sources, and these will be indicated in the output."),
+	checkboxInput("milk", "Milk", FALSE),
+checkboxInput("eggs", "Eggs", FALSE),
+checkboxInput("nuts", "Nuts", FALSE),
+checkboxInput("peanuts", "Peanuts", FALSE),
+checkboxInput("vegan", "Vegan", TRUE),
+	hr(),
+	textAreaInput("ingredients", "Paste ingredients here, seperated by commas.", value = "Beats, Bears, Battlestar Galactica."),
+	hr(),
+	actionButton("ex", "Give me an example!"),
+	hr(),
+	helpText("Note: This information is by no means guarenteed to be correct and is only meant to help you make an informed decision about which products you choose to consume and use.")
   ),
   mainPanel(
-	DT::dataTableOutput("table")
+	 tabsetPanel(
+	tabPanel("Check for allergens", DT::dataTableOutput("table")),
+	tabPanel("About this app", h3("A little more about the app"), p("Here I'm going to write a little bit more about this app"), h3("Contributing"), p("Here I'm going to write how to contribute"))
+	)
 	)
 )
